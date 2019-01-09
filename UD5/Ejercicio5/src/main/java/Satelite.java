af12e963e096544f9075f7e68fcfca6f070ad897
IN1DM3B_17 <IN1DM3B_17@H015112.NET>
2019-01-09 13:24:17 +0100

    adasdasd

--- UD5/Ejercicio5/src/main/java/Satelite.java
+++ UD5/Ejercicio5/src/main/java/Satelite.java
@@ -24,5 +24,8 @@ public class Satelite {
 	public void printPosicion(){
 		System.out.println("El satélite se encuentra en el paralelo "+ this.paralelo+ "Meridiano "+this.meridiano+" a una distancia de la tierra de "+this.distancia_tierra+"Kilómetros");
 	}
+	public void variarAltura(double desplazamiento) {
+		distancia_tierra=distancia_tierra+desplazamiento;
+	}
 }
 
